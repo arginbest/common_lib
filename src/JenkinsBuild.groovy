e45fc991938434fe4447220819f56923904ed2b6
arginbest <bsiitov84@gmail.com>
2020-12-12 19:02:22 +0000

    cf

--- src/JenkinsBuild.groovy
+++ src/JenkinsBuild.groovy
@@ -1,23 +1,3 @@
-      import jenkins.model.Jenkins
-      import hudson.security.SecurityRealm
-      import org.jenkinsci.plugins.GithubSecurityRealm
-      import jenkins.plugins.git.GitSCMSource
-      import jenkins.plugins.git.traits.BranchDiscoveryTrait
-      import org.jenkinsci.plugins.workflow.libs.GlobalLibraries
-      import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
-      import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
-      import net.sf.json.JSONObject
-      import hudson.*
-      import hudson.security.*
-      import jenkins.model.*
-      import java.util.*
-      import com.michelin.cio.hudson.plugins.rolestrategy.*
-      import com.synopsys.arc.jenkins.plugins.rolestrategy.*
-      import java.lang.reflect.*
-      import java.util.logging.*
-      import groovy.json.*
-      import groovy.json.JsonSlurper
-      import jenkins.model.Jenkins
 def call() { 
         node {
         stage ("succuss") {
