781511d48e27a35f59924428412a7f10ff8be67f
arginbest <bsiitov84@gmail.com>
2020-12-12 20:05:27 +0000

    add file

---
+++ checksuccuss.groovy
@@ -0,0 +1,9 @@
+@Library('common_lib') _ 
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.*
+import java.net.URL
+pipeline()
